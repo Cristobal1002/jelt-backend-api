1d134ba4fd326b77e9227264481d09366981cdce
Mairon Andres Florez Alvarez <andresflorez0799@gmail.com>
2026-01-04 23:23:47 -0500

    control para registro de log de database

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,8 @@ import { Sequelize } from 'sequelize';
 import { config } from './index.js';
 import { logger } from '../utils/logger.js';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 const sqz = new Sequelize(
     config.db.name,
     config.db.user,
@@ -11,7 +13,9 @@ const sqz = new Sequelize(
         port: config.db.port,
         dialect: config.db.dialect,
         pool: config.db.pool,
-        logging: config.db.logging ? (msg) => logger.debug(msg) : false,
+        //logging: config.db.logging ? (msg) => logger.debug(msg) : false,
+        logging: isDev ? (sql) => logger.debug(sql) : false,
+        benchmark: true,
     }
 );
 
